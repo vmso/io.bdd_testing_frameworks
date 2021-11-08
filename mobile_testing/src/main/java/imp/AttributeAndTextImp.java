352dae5c5bc5270820a33a882b4dce5798276043
serhatozdursun <serhat.ozdursun@gmail.com>
2021-11-08 20:03:35 +0300

    Get text

---
+++ mobile_testing/src/main/java/imp/AttributeAndTextImp.java
@@ -0,0 +1,21 @@
+package imp;
+
+import com.thoughtworks.gauge.Step;
+import com.thoughtworks.gauge.datastore.ScenarioDataStore;
+import exceptions.FileNotFound;
+import helper.AttributeAndTextHelper;
+import io.cucumber.java.en.And;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AttributeAndTextImp extends AttributeAndTextHelper {
+    private final Logger log = LogManager.getLogger(AttributeAndTextImp.class);
+
+    @Step("Get <> element text and store it Scenario Store with <> key")
+    @And("Get {string} element text and store it Scenario Store with {string} key")
+    public void getTextAndStoreScenarioStore(String jsonKey, String storeKey) throws FileNotFound {
+        var text = getElmText(jsonKey);
+        ScenarioDataStore.put(jsonKey, text);
+        log.info("{} text got and and stored during scenario {}={}", jsonKey, storeKey, text);
+    }
+}
--- mobile_testing/src/main/java/imp/RotateScreenImp.java
+++ mobile_testing/src/main/java/imp/RotateScreenImp.java
@@ -2,18 +2,18 @@ package imp;
 
 import com.thoughtworks.gauge.Step;
 import helper.RotateScreenHelper;
-import io.cucumber.java.en_scouse.An;
+import io.cucumber.java.en.And;
 
 public class RotateScreenImp extends RotateScreenHelper {
 
     @Step("Rotate screen as landscape.")
-    @An("Rotate screen as landscape.")
+    @And("Rotate screen as landscape.")
     public void rotateLandscape() {
         rotateScreen("LANDSCAPE");
     }
 
-    @Step("Rotate the screen as  portrait.")
-    @An("Rotate the screen as  portrait.")
+    @Step("Rotate the screen as portrait.")
+    @And("Rotate the screen as portrait.")
     public void rotatePortrait() {
         rotateScreen("PORTRAIT");
     }
