1307890da18dc1ffab483744c022ac5e6188c7b2
serhatozdursun <serhat.ozdursun@gmail.com>
2021-12-09 00:09:02 +0300

    realse version

--- web_testing/src/test/java/helpers/DataTableTests.java
+++ web_testing/src/test/java/helpers/DataTableTests.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DataTableTests {
@@ -21,7 +23,6 @@ public class DataTableTests {
         base.setUp();
         driver = DriverManager.getInstances().getDriver();
         GetFileName.getInstance().setFileName("examples");
-
     }
 
     @Test
@@ -34,9 +35,73 @@ public class DataTableTests {
         var table4 = tableHelper.getTableAsMapList("rowLocator", "headerLocator");
 
         assertEquals(table1, table2);
-        assertEquals(table2,table3);
-        assertEquals(table3,table4);
+        assertEquals(table2, table3);
+        assertEquals(table3, table4);
     }
+
+
+    @Test
+    public void findMax() {
+
+        driver.get("http://demo.guru99.com/test/web-table-element.php");
+
+        var cellSize = driver.findElements(By.cssSelector("td")).size();
+        var prevCloses = new ArrayList<String>();
+        for (int i = 3; i <= cellSize; i += 5) {
+            var prevClose = driver.findElement(By.xpath("(//td)[" + i + "]")).getText();
+            prevCloses.add(prevClose);
+        }
+
+        var currentPrices = new ArrayList<String>();
+        for (int i = 4; i <= cellSize; i += 5) {
+            var currentPrice = driver.findElement(By.xpath("(//td)[" + i + "]")).getText();
+            currentPrices.add(currentPrice);
+        }
+
+        var changes = new ArrayList<String>();
+        for (int i = 5; i <= cellSize; i += 5) {
+            var change = driver.findElement(By.xpath("(//td)[" + i + "]")).getText();
+            change = change.replaceAll("^[a-zA-Z:+-]", "").strip();
+            changes.add(change);
+        }
+
+        double prevMax = findMaxDouble(prevCloses);
+        double currMax = findMaxDouble(currentPrices);
+        double changeMax = findMaxDouble(changes);
+
+        double prevMin = findMinDouble(prevCloses);
+        double currMin = findMinDouble(currentPrices);
+        double changeMin = findMinDouble(changes);
+
+        System.out.printf("Prev max is %,.2f%n", prevMax);
+        System.out.printf("Current max is %,.2f%n", currMax);
+        System.out.printf("Change max is %%%,.1f percent%n", changeMax);
+
+        System.out.printf("Prev min is %,.2f%n", prevMin);
+        System.out.printf("Current min is %,.2f%n", currMin);
+        System.out.printf("Change min is %%%,.1f percent%n", changeMin);
+    }
+
+    private double findMaxDouble(ArrayList<String> list) {
+        double max = 0;
+        for (var priceText : list) {
+            var price = Double.parseDouble(priceText);
+            if (price > max)
+                max = price;
+        }
+        return max;
+    }
+
+    private double findMinDouble(ArrayList<String> list) {
+        double min = 99999999;
+        for (var priceText : list) {
+            var price = Double.parseDouble(priceText);
+            if (price < min)
+                min = price;
+        }
+        return min;
+    }
+
 
     @AfterEach
     public void tearDown() {
