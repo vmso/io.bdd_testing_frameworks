77dde53dd4ab2f581164f57423878e65a7aedc84
serhatozdursun <serhat.ozdursun@gmail.com>
2021-12-08 11:08:48 +0300

    ClickImp created and ClickHelper updated

--- web_testing/src/main/java/helpers/ClickHelper.java
+++ web_testing/src/main/java/helpers/ClickHelper.java
@@ -1,36 +1,47 @@
 package helpers;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 
 public class ClickHelper extends GetElementHelper {
-
+    private Logger log = LogManager.getLogger(ClickHelper.class);
+    private final String LOG_INFO= "Clicked on {}";
     public void clickElement(String jsonKey) {
         clickElement(jsonKey, DEFAULT_WAIT, DEFAULT_SLEEP_IN_MILLIS);
+        log.info(LOG_INFO,jsonKey);
     }
 
     public void clickElement(String jsonKey, long timeout) {
         clickElement(jsonKey, timeout, DEFAULT_SLEEP_IN_MILLIS);
+        log.info(LOG_INFO,jsonKey);
     }
 
     public void clickElement(String jsonKey, long timeout, long sleepMillis) {
         getClickableElement(jsonKey, timeout, sleepMillis).click();
+        log.info(LOG_INFO,jsonKey);
     }
 
     public void clickElement(By by) {
         getClickableElement(by).click();
+        log.info(LOG_INFO,by);
+
     }
 
     public void clickElement(By by, long timeout) {
         getClickableElement(by, timeout).click();
+        log.info(LOG_INFO,by);
     }
 
     public void clickElementWithoutWait(String jsonKey) {
         getElementsWithoutWait(jsonKey);
+        log.info(LOG_INFO,jsonKey);
     }
 
     public void clickElementWithoutWait(By by) {
         getElementsWithoutWait(by);
+        log.info(LOG_INFO,by);
     }
 
     public Boolean checkIfExitsClick(By by, long timeout) {
---
+++ web_testing/src/main/java/imp/ClickImp.java
@@ -0,0 +1,50 @@
+package imp;
+
+import com.thoughtworks.gauge.Step;
+import helpers.ClickHelper;
+import io.cucumber.java.en.And;
+
+public class ClickImp extends ClickHelper {
+
+    @And("Click on {string}")
+    @Step("Click on <string>")
+    public void clickImp(String jsonKey) {
+        clickElement(jsonKey);
+    }
+
+    @And("Click on {string} without wait")
+    @Step("Click on <string> without wait")
+    public void clickWithOutWaitImp(String jsonKey) {
+        clickElementWithoutWait(jsonKey);
+    }
+
+    @And("Wait for {long} second presence of <string> then click")
+    @Step("Wait for {int} second presence of <element> then click")
+    public void clickImp(String jsonKey, long timeout) {
+        clickElement(jsonKey, timeout);
+    }
+
+    @And("Wait for {long} second with sleep in {long} millis presence of <string> then click")
+    @Step("Wait for {long} second with sleep in {long} millis presence of <string> then click")
+    public void clickImp(String jsonKey, long timeout, long sleepInMillis) {
+        clickElement(jsonKey, timeout, sleepInMillis);
+    }
+
+    @And("Click if exists if so click on {string}")
+    @Step("Click if exists if so click on <element>")
+    public void clickIfExistsImp(String jsonKey) {
+        checkIfExitsClick(jsonKey);
+    }
+
+    @And("Click if exists if so click on {string}, if not click {string}")
+    @Step("Click if exists on <element>if not click <string>")
+    public void ifExistsClickXIfNotClickYImp(String xJsonKey, String yJsonKey) {
+        ifExistsClickXIfNotClickY(xJsonKey, yJsonKey);
+    }
+
+    @And("Wait for {long} second presence of {string} then click, if not exists click {string}")
+    @Step("Wait for {long} second presence of {string} then click, if not exists click {string}")
+    public void ifExistsClickXIfNotClickYImp(long timeout, String xJsonKey, String yJsonKey) {
+        ifExistsClickXIfNotClickY(xJsonKey, yJsonKey, timeout);
+    }
+}
